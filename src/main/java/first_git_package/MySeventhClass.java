cb972bec1228331d36580ae9287dcdf9ab93c099
Mehmet Okay <mehmetokay34@icloud.com>
2023-03-12 17:01:18 +0100

    commiting my seventhclas

---
+++ src/main/java/first_git_package/MySeventhClass.java
@@ -0,0 +1,13 @@
+package first_git_package;
+
+public class MySeventhClass {
+    public static void main(String[] args) {
+
+        System.out.println("My seventh class");
+        //Then new code will be in remote mehmet branch
+        //Then in github expert or us will do:
+        // Pull Request (PR)(US or Expert)
+        //Then code
+
+    }
+}

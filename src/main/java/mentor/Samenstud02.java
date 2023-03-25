b7287ace7bc667829d46a0157d264d13b3a652bb
Mehmet Okay <mehmetokay34@icloud.com>
2023-03-25 10:35:21 +0100

    update

---
+++ src/main/java/mentor/Samenstud02.java
@@ -0,0 +1,24 @@
+package mentor;
+
+public class Samenstud02 {
+    public static void main(String[] args) {
+
+    }
+    /*
+
+    Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true
+
+     */
+    public boolean posNeg(int a, int b, boolean negative) {
+        boolean negative;
+        int a=-1;
+        int b=1;
+
+
+    }
+
+
+}
